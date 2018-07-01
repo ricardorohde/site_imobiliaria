41d9dfc17cf48af336158a2baf366d31aaf503e7
karencplara <34969669+karencplara@users.noreply.github.com>
2018-07-01 14:15:16 -0300

    Initial commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+Os comandos do prompt são para configurar o laravel.
+o make:auth serve para ativar o login.
---
+++ resources/views/layouts/site.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{asset('lib/materialize/dist/css/materialize.css')}}">
+    <!-- asset já reconhece public -->
+    <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+<body>
+    <div id="app">
+        <nav>
+            <div class="nav-wrapper blue">
+             <div class="container">
+                  <a href="#!" class="brand-logo">Site</a>
+                  <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+                  <ul class="right hide-on-med-and-down">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Contato</a></li>
+                  </ul>
+                   <ul class="sidenav" id="mobile-demo">
+                    
+                   </ul>
+               </div>
+            </div>
+        </nav>
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+    <script src="{{asset('lib/jquery/dist/jquery.js')}}"></script>
+    <script src="{{asset('lib/materialize/dist/js/materialize.js')}}"></script>
+    <script src="{{asset('js/init.js')}}"></script>
+</body>
+</html>
