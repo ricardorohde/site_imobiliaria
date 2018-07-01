a60e3c609b7cf860b80e9200731e1ff07309fae4
karencplara <34969669+karencplara@users.noreply.github.com>
2018-07-01 17:54:08 -0300

    alt commit
    Nova página

--- resources/views/layouts/_site/_lista_imoveis.blade.php
+++ resources/views/layouts/_site/_lista_imoveis.blade.php
@@ -1,5 +1,138 @@
 <section>
   <div class="row">
-	<h3 align="center"></h3>
+	<h3 align="center">Imóveis</h3>
+	<div class="divider"></div>
   </div>
+</section>
+<section>
+  <div class="row">
+	<div class="col s12 m3 l3">
+		<div class="card">
+			<div class="card-image">
+				<a href="#"><img src="{{ asset('img/img1.jpg')}}" align="Imagem do imóvel"></a>
+			</div>
+			<div class="card-content">
+				<p><b class="deep-orange-text darken-1">Vende</b></p>
+				<p><b>Título do imóvel</b></p>
+				<p>Descrição do imóvel</p>
+				<p>R$300.000,00</p>
+			</div>
+			<div class="card-action">
+				<a href="#">Ver mais...</a>
+			</div>
+		</div>
+	</div>
+	<div class="col s12 m3 l3">
+		<div class="card">
+			<div class="card-image">
+				<a href="#"><img src="{{ asset('img/img1.jpg')}}" align="Imagem do imóvel"></a>
+			</div>
+			<div class="card-content">
+				<p><b class="deep-orange-text darken-1">Vende</b></p>
+				<p><b>Título do imóvel</b></p>
+				<p>Descrição do imóvel</p>
+				<p>R$300.000,00</p>
+			</div>
+			<div class="card-action">
+				<a href="#">Ver mais...</a>
+			</div>
+		</div>
+	</div>
+	<div class="col s12 m3 l3">
+		<div class="card">
+			<div class="card-image">
+				<a href="#"><img src="{{ asset('img/img1.jpg')}}" align="Imagem do imóvel"></a>
+			</div>
+			<div class="card-content">
+				<p><b class="deep-orange-text darken-1">Vende</b></p>
+				<p><b>Título do imóvel</b></p>
+				<p>Descrição do imóvel</p>
+				<p>R$300.000,00</p>
+			</div>
+			<div class="card-action">
+				<a href="#">Ver mais...</a>
+			</div>
+		</div>
+	</div>
+	<div class="col s12 m3 l3">
+		<div class="card">
+			<div class="card-image">
+				<a href="#"><img src="{{ asset('img/img1.jpg')}}" align="Imagem do imóvel"></a>
+			</div>
+			<div class="card-content">
+				<p><b class="deep-orange-text darken-1">Vende</b></p>
+				<p><b>Título do imóvel</b></p>
+				<p>Descrição do imóvel</p>
+				<p>R$300.000,00</p>
+			</div>
+			<div class="card-action">
+				<a href="#">Ver mais...</a>
+			</div>
+		</div>
+	</div>
+	<div class="col s12 m3 l3">
+		<div class="card">
+			<div class="card-image">
+				<a href="#"><img src="{{ asset('img/img1.jpg')}}" align="Imagem do imóvel"></a>
+			</div>
+			<div class="card-content">
+				<p><b class="deep-orange-text darken-1">Vende</b></p>
+				<p><b>Título do imóvel</b></p>
+				<p>Descrição do imóvel</p>
+				<p>R$300.000,00</p>
+			</div>
+			<div class="card-action">
+				<a href="#">Ver mais...</a>
+			</div>
+		</div>
+	</div>
+	<div class="col s12 m3 l3">
+		<div class="card">
+			<div class="card-image">
+				<a href="#"><img src="{{ asset('img/img1.jpg')}}" align="Imagem do imóvel"></a>
+			</div>
+			<div class="card-content">
+				<p><b class="deep-orange-text darken-1">Vende</b></p>
+				<p><b>Título do imóvel</b></p>
+				<p>Descrição do imóvel</p>
+				<p>R$300.000,00</p>
+			</div>
+			<div class="card-action">
+				<a href="#">Ver mais...</a>
+			</div>
+		</div>
+	</div>
+	<div class="col s12 m3 l3">
+		<div class="card">
+			<div class="card-image">
+				<a href="#"><img src="{{ asset('img/img1.jpg')}}" align="Imagem do imóvel"></a>
+			</div>
+			<div class="card-content">
+				<p><b class="deep-orange-text darken-1">Vende</b></p>
+				<p><b>Título do imóvel</b></p>
+				<p>Descrição do imóvel</p>
+				<p>R$300.000,00</p>
+			</div>
+			<div class="card-action">
+				<a href="#">Ver mais...</a>
+			</div>
+		</div>
+	</div>
+	<div class="col s12 m3 l3">
+		<div class="card">
+			<div class="card-image">
+				<a href="#"><img src="{{ asset('img/img1.jpg')}}" align="Imagem do imóvel"></a>
+			</div>
+			<div class="card-content">
+				<p><b class="deep-orange-text darken-1">Vende</b></p>
+				<p><b>Título do imóvel</b></p>
+				<p>Descrição do imóvel</p>
+				<p>R$300.000,00</p>
+			</div>
+			<div class="card-action">
+				<a href="#">Ver mais...</a>
+			</div>
+		</div>
+	</div>
+ </div>
 </section>
